6dbda6ded50b62131d80321c13a08d59630380ac
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-02-11 22:59:29 +0700

    (⌐■_■)

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import tw from 'twrnc'
+
+export default function Index() {
+  return (
+    <View
+      style={tw`flex-1 justify-center items-center`}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
